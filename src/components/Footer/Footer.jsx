71633b62ecdce59d92ebef57bb6daeaa8aaa3f72
Andrey Roditelev <rodandr13@gmail.com>
2023-08-30 01:05:17 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p className='footer__caption'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <p className='footer__copyright'>© 2020</p>
+            <ul className='footer__list'>
+                <li className='footer__item'><a className='footer__link' href="#">Яндекс.Практикум</a></li>
+                <li className='footer__item'><a className='footer__link' href="#">Github</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
