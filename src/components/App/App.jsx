5619e8f167a235e6e0b6959001fce864bec8605c
Andrey Roditelev <rodandr13@gmail.com>
2023-09-02 23:28:26 +0300

    feat: add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Login from '../Login/Login';
-
-// eslint-disable-next-line no-lone-blocks
-{ /*
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
 import Register from '../Register/Register';
 import Header from '../Header/Header';
-import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies'; */ }
+import SavedMovies from '../SavedMovies/SavedMovies';
 
 function App() {
   return (
     <div className="page">
-      <Login />
-
-      {/*  <Register />
       <Header />
-        <Main />
-        <Movies />
-        <SavedMovies />
-
-        <Footer /> */}
-
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+      </Routes>
+      <Footer />
+      <Login />
     </div>
   );
 }
