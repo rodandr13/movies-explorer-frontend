87ba8f9a07777d4e99341d15e31c0bff6b370498
Andrey Roditelev <rodandr13@gmail.com>
2023-09-05 13:43:14 +0300

    feat: add media styles for movies

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './MoviesCardList.css';
-import movieImage from '../../images/movie-img.jpg';
+import movieImage1 from '../../images/movie-img.jpg';
+import movieImage2 from '../../images/movie-img-2.jpg';
+import movieImage3 from '../../images/movie-img-3.jpg';
+import movieImage4 from '../../images/movie-img-4.jpg';
 
 function MoviesCardList() {
   return (
     <section className="movies-card-list">
       <ul className="movies-card-list__list">
         <li className="movies-card movies-card-list__item">
-          <img className="movies-card__image" src={movieImage} alt="" />
+          <img className="movies-card__image" src={movieImage1} alt="" />
           <div className="movies-card__container">
             <h3 className="movies-card__title">33 слова о дизайне</h3>
             <button className="movies-card__like" type="button" aria-label="Нравится" />
@@ -15,7 +18,31 @@ function MoviesCardList() {
           <p className="movies-card__duration">1ч 42м</p>
         </li>
         <li className="movies-card movies-card-list__item">
-          <img className="movies-card__image" src={movieImage} alt="" />
+          <img className="movies-card__image" src={movieImage2} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">Киноальманах «100 лет дизайна»</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage3} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">В погоне за Бенкси</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage4} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">Баския: Взрыв реальности</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
           <div className="movies-card__container">
             <h3 className="movies-card__title">33 слова о дизайне</h3>
             <button className="movies-card__like" type="button" aria-label="Нравится" />
@@ -23,7 +50,7 @@ function MoviesCardList() {
           <p className="movies-card__duration">1ч 42м</p>
         </li>
         <li className="movies-card movies-card-list__item">
-          <img className="movies-card__image" src={movieImage} alt="" />
+          <img className="movies-card__image" src={movieImage1} alt="" />
           <div className="movies-card__container">
             <h3 className="movies-card__title">33 слова о дизайне</h3>
             <button className="movies-card__like" type="button" aria-label="Нравится" />
@@ -31,7 +58,7 @@ function MoviesCardList() {
           <p className="movies-card__duration">1ч 42м</p>
         </li>
         <li className="movies-card movies-card-list__item">
-          <img className="movies-card__image" src={movieImage} alt="" />
+          <img className="movies-card__image" src={movieImage1} alt="" />
           <div className="movies-card__container">
             <h3 className="movies-card__title">33 слова о дизайне</h3>
             <button className="movies-card__like" type="button" aria-label="Нравится" />
@@ -39,7 +66,55 @@ function MoviesCardList() {
           <p className="movies-card__duration">1ч 42м</p>
         </li>
         <li className="movies-card movies-card-list__item">
-          <img className="movies-card__image" src={movieImage} alt="" />
+          <img className="movies-card__image" src={movieImage1} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">Gimme Danger: История Игги и The Stooges</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">33 слова о дизайне</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">33 слова о дизайне</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">33 слова о дизайне</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">33 слова о дизайне</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
+          <div className="movies-card__container">
+            <h3 className="movies-card__title">33 слова о дизайне</h3>
+            <button className="movies-card__like" type="button" aria-label="Нравится" />
+          </div>
+          <p className="movies-card__duration">1ч 42м</p>
+        </li>
+        <li className="movies-card movies-card-list__item">
+          <img className="movies-card__image" src={movieImage1} alt="" />
           <div className="movies-card__container">
             <h3 className="movies-card__title">33 слова о дизайне</h3>
             <button className="movies-card__like" type="button" aria-label="Нравится" />
