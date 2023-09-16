ca21b94abbe6bf99b4c25adfc756241f89109c65
Andrey Roditelev <rodandr13@gmail.com>
2023-09-16 14:29:19 +0300

    feat: add short movies filter

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 // import { movies } from '../../utils/constans';
@@ -9,26 +9,41 @@ function Movies() {
   const [movies, setMovies] = useState([]);
   const [searchError, setSearchError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-
+  const savedIsShortFilm = localStorage.getItem('isShortFilm');
+  const [isShortFilm, setIsShortFilm] = useState(
+    savedIsShortFilm ? JSON.parse(savedIsShortFilm) : false,
+  );
+  const savedQuery = localStorage.getItem('query');
+  const [query, setQuery] = useState(savedQuery || '');
   useEffect(() => {
-    const savedMovies = localStorage.getItem('savedMovies');
-    if (savedMovies) {
-      setMovies(JSON.parse(savedMovies));
+    localStorage.setItem('isShortFilm', JSON.stringify(isShortFilm));
+  }, [isShortFilm]);
+  useEffect(() => {
+    localStorage.setItem('query', query);
+  }, [query]);
+  useEffect(() => {
+    let currentMovies = JSON.parse(localStorage.getItem('savedMovies') || '[]');
+    if (isShortFilm) {
+      currentMovies = currentMovies.filter((movie) => movie.duration <= 40);
     }
-  }, []);
-
-  const handleSubmit = (query) => {
-    if (!query.trim()) {
+    setMovies(currentMovies);
+  }, [isShortFilm]);
+  const handleSubmit = (submittedQuery, currentIsShortFilm) => {
+    if (!submittedQuery.trim()) {
       setSearchError('Нужно ввести ключевое слово');
       return;
     }
+    setQuery(submittedQuery);
     setIsLoading(true);
     getMovies()
       .then((allMovies) => {
-        const filteredMovies = allMovies.filter(
-          (movie) => movie.nameRU.toLowerCase().includes(query.toLowerCase())
-            || movie.nameEN.toLowerCase().includes(query.toLowerCase()),
+        let filteredMovies = allMovies.filter(
+          (movie) => movie.nameRU.toLowerCase().includes(submittedQuery.toLowerCase())
+            || movie.nameEN.toLowerCase().includes(submittedQuery.toLowerCase()),
         );
+        if (currentIsShortFilm) {
+          filteredMovies = filteredMovies.filter((movie) => movie.duration <= 40);
+        }
         if (filteredMovies.length === 0) {
           setSearchError('Ничего не найдено');
           return;
@@ -44,10 +59,31 @@ function Movies() {
         setIsLoading(false);
       });
   };
+  useEffect(() => {
+    const savedMovies = localStorage.getItem('savedMovies');
+    if (savedMovies) {
+      setMovies(JSON.parse(savedMovies));
+    }
+    if (query) {
+      handleSubmit(query, isShortFilm);
+    }
+  }, []);
+  const handleFilterChangeCallback = useCallback((newIsShortFilm) => {
+    setIsShortFilm(newIsShortFilm);
+    if (query) {
+      handleSubmit(query, newIsShortFilm);
+    }
+  }, [query, handleSubmit]);
   return (
     <main className="movies">
-      <SearchForm onSubmit={handleSubmit} />
-      {isLoading ? <Preloader /> : <MoviesCardList movies={movies} searchError={searchError} /> }
+      <SearchForm
+        onSubmit={handleSubmit}
+        isShortFilm={isShortFilm}
+        setIsShortFilm={setIsShortFilm}
+        query={query}
+        onFilterChange={handleFilterChangeCallback}
+      />
+      {isLoading ? <Preloader /> : <MoviesCardList movies={movies} searchError={searchError} />}
     </main>
   );
 }
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,24 +2,36 @@ import React, { useState } from 'react';
 import './SearchForm.css';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
-function SearchForm({ onSubmit }) {
-  const [query, setQuery] = useState('');
+function SearchForm({
+  onSubmit, isShortFilm, setIsShortFilm, query, onFilterChange,
+}) {
+  const [inputQuery, setInputQuery] = useState(query || '');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit(query);
+    onSubmit(inputQuery);
   };
   const handleChange = (e) => {
-    setQuery(e.target.value);
+    setInputQuery(e.target.value);
   };
   return (
     <section className="search-form">
       <form className="search-form__form" onSubmit={handleSubmit}>
         <div className="search-form__container">
-          <input className="search-form__input" type="text" placeholder="Фильм" value={query} onChange={handleChange} />
+          <input
+            className="search-form__input"
+            type="text"
+            placeholder="Фильм"
+            value={inputQuery}
+            onChange={handleChange}
+          />
           <button className="search-form__submit" type="submit">Найти</button>
         </div>
-        <FilterCheckbox />
+        <FilterCheckbox
+          isShortFilm={isShortFilm}
+          setIsShortFilm={setIsShortFilm}
+          onFilterChange={onFilterChange}
+        />
       </form>
     </section>
   );
