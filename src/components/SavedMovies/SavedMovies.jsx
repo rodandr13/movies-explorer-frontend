baa0ae5e869b465fd7879feef6e6e51ac37afc92
Andrey Roditelev <rodandr13@gmail.com>
2023-09-22 15:13:30 +0300

    fix: edit saved movies

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import { filterMovies, getFromLocalStorage, setToLocalStorage } from '../../utils/utils';
+import { filterMovies } from '../../utils/utils';
 import {
   ENTER_KEYWORD_MESSAGE,
   NOTHING_FOUND_MESSAGE,
-  LOCAL_STORAGE_KEYS,
 } from '../../utils/constants';
 
 function SavedMovies({ movies, handleDeleteMovie }) {
   const [filteredMovies, setFilteredMovies] = useState(movies);
   const [searchError, setSearchError] = useState('');
-  const savedIsShortFilm = getFromLocalStorage(LOCAL_STORAGE_KEYS.SAVED_IS_SHORT_FILM, false);
-  const [isShortFilm, setIsShortFilm] = useState(savedIsShortFilm);
-
-  const savedQuery = getFromLocalStorage(LOCAL_STORAGE_KEYS.SAVED_QUERY, '');
-  const [query, setQuery] = useState(savedQuery);
+  const [isShortFilm, setIsShortFilm] = useState(false);
+  const [query, setQuery] = useState('');
 
   const applyFilters = useCallback((queryValue, isShortFilmValue) => {
     const resultMovies = filterMovies(movies, queryValue, isShortFilmValue);
@@ -40,20 +36,6 @@ function SavedMovies({ movies, handleDeleteMovie }) {
     setQuery(submittedQuery);
     applyFilters(submittedQuery, currentIsShortFilm);
   };
-
-  useEffect(() => {
-    setToLocalStorage(LOCAL_STORAGE_KEYS.SAVED_IS_SHORT_FILM, isShortFilm);
-    setToLocalStorage(LOCAL_STORAGE_KEYS.SAVED_QUERY, query);
-  }, [isShortFilm, query]);
-
-  useEffect(() => {
-    if (savedQuery || savedIsShortFilm) {
-      applyFilters(savedQuery, JSON.parse(savedIsShortFilm));
-    } else {
-      setFilteredMovies(movies);
-      setSearchError('');
-    }
-  }, [movies, savedQuery, savedIsShortFilm, applyFilters]);
 
   const handleFilterChangeCallback = useCallback((newIsShortFilm) => {
     setIsShortFilm(newIsShortFilm);
